76725e26b2c3648722bbefdc67a464f4ddb4de69
Warren Clough <clough.warren@gmail.com>
2013-03-08 00:44:42 -0500

    fixed issue with the environment

--- application/config/database.php
+++ application/config/database.php
@@ -69,10 +69,10 @@ return array(
 
 		'mysql' => array(
 			'driver'   => 'mysql',
-			'host'     => '127.0.0.1',
-			'database' => 'laravel',
-			'username' => 'root',
-			'password' => 'gtx@6075',
+			'host'     => 'tunnel.pagodabox.com',
+			'database' => 'the-students-portal',
+			'username' => 'celestina',
+			'password' => 'dYQZuKHc',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 		),
